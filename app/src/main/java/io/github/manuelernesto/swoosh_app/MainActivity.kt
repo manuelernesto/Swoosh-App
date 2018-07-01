99627c1612e81ed3c7552607cd4625f1aeb61933
Manuel Ernesto <manuelernesto@MacBook-Pro-de-Manuel.local>
2018-07-02 00:01:23 +0100

    Finish The design of the league screen

--- app/src/main/java/io/github/manuelernesto/swoosh_app/MainActivity.kt
+++ app/src/main/java/io/github/manuelernesto/swoosh_app/MainActivity.kt
@@ -1,12 +1,19 @@
 package io.github.manuelernesto.swoosh_app
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnstarted.setOnClickListener{
+            val intent = Intent(this, LeagueActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
